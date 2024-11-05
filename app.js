615f79810f4fbf993e8f5c4a180b8eda7fbf5d83
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-05 17:54:17 +0100

    ADD generate mail function

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-axios
-  .get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then((res) => {
-    console.log(res.data, "Questo è il dato restituito");
-  })
-  .catch((err) => {
-    console.log(err, "Qualcosa è andato storto!");
-  });
+let userMails = [];
+const mailList = document.querySelector("mail-list");
+
+function generateTenEmails() {
+  for (let i = 0; i < 10; i++)
+    axios
+      //questa è la chiamata alla API
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      //questa è la risposta
+      .then((res) => {
+        // console.log(res.data, "Questo è il dato restituito");
+
+        const mail = res.data.response;
+        // console.log(mail);
+        userMails.push(mail);
+      })
+      //questa è risposta in caso di errore
+      .catch((err) => {
+        console.log(err, "Qualcosa è andato storto!");
+      });
+}
+
+generateTenEmails();
+console.log(userMails);
