5f1e5275f31d2364d9d3595b520453ff6f6cf637
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-05 17:05:54 +0100

    ADD console log to .then & .catch

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 axios
   .get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then((res) => {
-    console.log(res.data);
+    console.log(res.data, "Questo è il dato restituito");
+  })
+  .catch((err) => {
+    console.log(err, "Qualcosa è andato storto!");
   });
