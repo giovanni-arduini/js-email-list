eab6a2f99820a907f9938a42c0409ecb42a5f83e
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-06 09:57:20 +0100

    ADD button

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 let userMails = [];
-const mailList = document.querySelector("mail-list");
+const mailList = document.getElementById("mail-list");
+// console.log(mailList);
+const buttonElement = document.querySelector(".btn");
+console.log(buttonElement);
 
 function generateTenEmails() {
-  for (let i = 0; i < 10; i++)
+  for (let i = 0; i < 10; i++) {
     axios
       //questa è la chiamata alla API
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
       //questa è la risposta
       .then((res) => {
-        // console.log(res.data, "Questo è il dato restituito");
-
         const mail = res.data.response;
-        // console.log(mail);
-        userMails.push(mail);
+        mailList.innerHTML += `<li>${mail}</li>`;
       })
+
       //questa è risposta in caso di errore
       .catch((err) => {
         console.log(err, "Qualcosa è andato storto!");
       });
+  }
 }
 
 generateTenEmails();
-console.log(userMails);
+// console.log(userMails);
+
+// let listElement = document.createElement("li");
+// listElement.innerHTML = `userMails[2]`;
+// mailList.appendChild(listElement);
+
+buttonElement.addEventListener("click", () => {
+  generateTenEmails();
+});
